dc295caca1f8741e7dc7addabcb746d6e2016832
program-perfect <is.my.gym.is.my@gmail.com>
2026-06-05 16:29:18 +0300

    feat: load fluid cursor styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import './glow.css'
+import './cursor.css'
 
 const geistSans = Geist({ subsets: ['latin'], variable: '--font-geist-sans' })
 const geistMono = Geist_Mono({ subsets: ['latin'], variable: '--font-geist-mono' })
